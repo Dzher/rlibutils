cc208e296d9530bb33eeb10731744b19837d55cc
Dzher <2539925009@qq.com>
2025-06-04 16:36:47 +0800

    adjust forward cout for inverse use

--- tests/common.h
+++ tests/common.h
@@ -12,13 +12,13 @@ inline void test_forward(const roblib::Motioner& motioner, const std::vector<dou
         auto endless_trans = res.value();
         std::cout << "Position: " << std::endl;
         for (auto each : endless_trans.position()) {
-            std::cout << each << ", ";
+            std::cout << each << " ";
         }
         std::cout << std::endl;
 
-        std::cout << "rotation: " << std::endl;
+        std::cout << "Rotation: " << std::endl;
         for (auto each : endless_trans.rotation()) {
-            std::cout << each << ", ";
+            std::cout << each << " ";
         }
         std::cout << std::endl
                   << std::endl;
@@ -31,13 +31,15 @@ inline void test_inverse(const roblib::Motioner& motioner, const roblib::xyzWith
 {
     auto res = motioner.getDegreesByXyzQuat(transpos);
     if (!res.has_value()) {
-        std::cout << "Inverse has no answer!";
+        std::cout << "Inverse Operation has NO Answer!";
         return;
     }
     std::cout << "inv" << std::endl;
     for (auto each : res.value()) {
-        std::cout << each << ", ";
+        std::cout << each << " ";
     }
+    std::cout << std::endl
+              << std::endl;
 }
 
 inline std::vector<double> readJoints(int dof)
