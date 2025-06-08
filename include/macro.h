f25a86a5db28099262cc42b2372694f1fb410f75
Dzher <2539925009@qq.com>
2025-06-08 18:59:02 +0800

    make rlib shared

--- include/macro.h
+++ include/macro.h
@@ -2,18 +2,18 @@
 
 /*====================================================================*/
 // when use shared library
-// #ifdef _WIN32
-//     #ifdef R_LIB_SHARED_EXPORT
-//         #define RLIB_API __declspec(dllexport)
-//     #else
-//         #define RLIB_API __declspec(dllimport)
-//     #endif
-// #else
-//     #define RLIB_API
-// #endif
+#ifdef _WIN32
+    #ifdef R_LIB_SHARED_EXPORT
+        #define RLIB_API __declspec(dllexport)
+    #else
+        #define RLIB_API __declspec(dllimport)
+    #endif
+#else
+    #define RLIB_API
+#endif
 /*====================================================================*/
 
 /*====================================================================*/
 // when use static library
-#define RLIB_API
+// #define RLIB_API
 /*====================================================================*/
