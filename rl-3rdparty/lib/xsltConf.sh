34f7d532f6c3b0b89bd243b9df7865ce6ac695bc
Dzher <2539925009@qq.com>
2025-06-20 10:14:28 +0800

    rm large files

--- rl-3rdparty/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-LD:/a/rl-3rdparty/rl-3rdparty-install/lib"
-XSLT_LIBS="-lxslt -lxml2"
-XSLT_PRIVATE_LIBS=" "
-XSLT_INCLUDEDIR="-ID:/a/rl-3rdparty/rl-3rdparty-install/include"
-MODULE_VERSION="xslt-1.1.43"
